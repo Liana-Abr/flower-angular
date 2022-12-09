727bb80fd2346d844483b2b6d80fe365a1f606ba
Liana <AbramyanLV20@st.ithub.ru>
2022-12-09 22:57:02 +0300

    POST plant

--- src/app/pages/AddPlant/addplant.component.ts
+++ src/app/pages/AddPlant/addplant.component.ts
@@ -8,51 +8,22 @@ import {Component} from "@angular/core";
 })
 
 export class AddPlant {
-
-
+ data : {name: string, description: string, images: string, category: string, sunlight: number, watering: string, temperature: string }[] = []
   constructor(private http: HttpClient){
 
   }
 
-  onPlantCreate(plants: {name: string, description: string, images: string[], category: string, sunlight: string, watering: string, temperature: string}){
-    console.log(plants);
-    const headers = new HttpHeaders({"Accept": "application/json",
-    "Content-Type": "application/json"});
-    this.http.post('https://api.petiteweb.dev/plants', plants, {headers: headers})
+  addPlant(self = this){
+    
+    const headers = new HttpHeaders({ "Accept": "application/json","Content-Type": "application/json"});
+    this.http.post(
+      'https://api.petiteweb.dev/plants', 
+      this.data, {headers: headers})
     .subscribe((res) =>{
       console.log(res);
-      
-    });
 
-    
+    });
   }
 
 
-
-  // data : {name: string, description: string, images: string[], category: string, sunlight: string, watering: string, temperature: string}[] = []
-
-
-  // addPlant(self = this){
-  //   let path:string = "https://api.petiteweb.dev/plants";
-  //   console.log(path)
-  //   const params: {[k: string]: string} = {
-  //     "name": "string",
-  //     "description": "string",
-  //     "images": "string",
-  //     "category": "string",
-  //     "sunlight": "string",
-  //     "watering": "string",
-  //     "temperature": "string",
-  //   }
-
-
-
-  //   fetch(path)
-  //     .then(function (res): any {
-  //       return res.json()
-  //     })
-  //     .then(function (d): any {
-  //       console.log(self.data)
-  //     })
-  // }
 }
